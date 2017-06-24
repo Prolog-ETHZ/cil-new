19a345ee39d3532e45fd224241f951775e6c8f30
Prolog-ETHZ <prolog949@gmail.com>
2017-06-24 17:00:11 +0200

    FUCK:

--- road_segmentation/VGG-16.py
+++ road_segmentation/VGG-16.py
@@ -688,9 +688,9 @@ def main(argv=None):  # pylint: disable=unused-argument
 
         p_data = numpy.asarray(refactored_data)
         data_node = tf.constant(p_data)
-
+        extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
         output = tf.nn.softmax(model(data_node,False))
-        output_prediction = s.run(output,feed_dict={keep_prob:1.0,phase_train:False})
+        _,output_prediction = s.run([extra_update_ops,output],feed_dict={keep_prob:1.0,phase_train:False})
 
         img_prediction = label_to_img(img.shape[0], img.shape[1], IMG_PATCH_SIZE, IMG_PATCH_SIZE, output_prediction)
 
