35f3e7839d59c910cd42c42943e0329effd1f588
Prolog-ETHZ <prolog949@gmail.com>
2017-06-26 03:50:24 +0200

    ROI

--- road_segmentation/mask_to_submission.py
+++ road_segmentation/mask_to_submission.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.image as mpimg
 import re
 
-foreground_threshold = 0.4 # percentage of pixels > 1 required to assign a foreground label to a patch
+foreground_threshold = 0.3 # percentage of pixels > 1 required to assign a foreground label to a patch
 
 # assign a label to a patch
 def patch_to_label(patch):
